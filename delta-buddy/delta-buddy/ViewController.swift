a7ba0b2e362fa93628220dc347eade68533643b7
Shoji Moto <shmoto1@gmail.com>
2017-10-14 15:06:25 -0400

    init xcode proj

---
+++ delta-buddy/delta-buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  delta-buddy
+//
+//  Created by Shoji Moto on 10/14/17.
+//  Copyright © 2017 Shoji Moto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
