e4330c027ed4012f453a9bf3834f51e0e3826b85
Shoji Moto <shmoto1@gmail.com>
2017-10-14 16:43:17 -0400

    swiftyjson imported and tsa data called

--- delta-buddy/delta-buddy/ViewController.swift
+++ delta-buddy/delta-buddy/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://deltaairlines-dev.apigee.net/v1/hack/waittime?airport=ORD")
+        var request = URLRequest(url: url!)
+        request.addValue("Bearer O68OqqGKNrb5EC2hEGE6YIVFGeae", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data else {return }
+            
+            let json = JSON(data: data)
+            print("data got")
+        }.resume()
     }
 
     override func didReceiveMemoryWarning() {
